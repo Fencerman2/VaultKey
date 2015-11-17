974cb0275819b06e1468dc18e6058d7b872831b5
Tristan <tristandwillis@gmail.com>
2015-11-17 16:43:46 -0500

    Updated Admin

--- vaultkey/cards/admin.py
+++ vaultkey/cards/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 
 from .models import Request
 # Register your models here.
-admin.site.register(Request)
+class RequestAdmin(admin.ModelAdmin)
+    fieldsets = [
+        (None,              {'fields': ['request_text']}),
+        ('Date information',{'fields': ['pub_date']}),
+    ]
+
+admin.site.register(Request, RequestAdmin)
