b070af86b84a32e35d0cbe478cb4811451cafdb7
Troy Smith <fencerman2@gmail.com>
2015-11-17 16:52:31 -0500

    syntax

--- vaultkey/cards/admin.py
+++ vaultkey/cards/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 from .models import Request
 # Register your models here.
-class RequestAdmin(admin.ModelAdmin)
+class RequestAdmin(admin.ModelAdmin):
     fieldsets = [
         (None,              {'fields': ['request_text']}),
         ('Date information',{'fields': ['pub_date']}),
