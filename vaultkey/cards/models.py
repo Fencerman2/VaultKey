6ee306c836f92d4c865e9d445c31b7d0367b87a9
Troy Smith <fencerman2@gmail.com>
2015-12-01 15:43:50 -0500

    created db defaults

--- vaultkey/cards/models.py
+++ vaultkey/cards/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 class Card(models.Model):
+    article_name = models.CharField(max_length=200)
+
     article_text = models.TextField()
 
     pub_date = models.DateTimeField('date submitted')
