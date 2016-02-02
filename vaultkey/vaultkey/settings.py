de4855e9ba128cfad8d180f802b2162f48768be7
Tristan <tristandwillis@gmail.com>
2016-02-02 16:27:11 -0500

    idk

--- vaultkey/vaultkey/settings.py
+++ vaultkey/vaultkey/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 try:
-    import * from secretsettings
+    from secretsettings import *
 except:
     pass
 
