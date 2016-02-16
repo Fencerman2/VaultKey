ab94e3c44b636b834e4c77c02312a3288ccefe08
Tristan <tristandwillis@gmail.com>
2016-02-16 16:54:58 -0500

    merge

--- vaultkey/vaultkey/settings.py
+++ vaultkey/vaultkey/settings.py
@@ -37,6 +37,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = (
     'material',
     'material.admin',
+    'material.frontend',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
