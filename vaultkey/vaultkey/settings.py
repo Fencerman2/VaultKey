5bfefcdc258f155125094d7b7341cec0ec22b083
Troy Smith <fencerman2@gmail.com>
2016-03-31 15:42:46 -0400

    added link from alters to contact

--- vaultkey/vaultkey/settings.py
+++ vaultkey/vaultkey/settings.py
@@ -37,7 +37,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = (
     'material',
     'material.admin',
-    'material.frontend',
+    'bootstrapped',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
