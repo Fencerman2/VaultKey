b92b625c4ee21c8be74e9427121190a843e2c9c4
Troy Smith <fencerman2@gmail.com>
2016-04-12 15:51:24 -0400

    removed outdated bootstrapped app

--- vaultkey/vaultkey/settings.py
+++ vaultkey/vaultkey/settings.py
@@ -37,7 +37,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = (
     'material',
     'material.admin',
-    'bootstrapped',
+    # 'bootstrapped',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
