27f06946c40ae86e85f955236002a1c6b911f4b5
Troy Smith <fencerman2@gmail.com>
2016-04-19 16:53:41 -0400

    added navbar to all pages

--- vaultkey/articles/urls.py
+++ vaultkey/articles/urls.py
@@ -3,7 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^$', views.ArchiveView.as_view(), name='archive'),
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='article-detail'),
-    url(r'^archive$', views.ArchiveView.as_view(), name='archive')
 ]
