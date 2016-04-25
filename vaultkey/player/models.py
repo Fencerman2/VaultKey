c884cc3c220d2e83f94f1e625dee318cb3ef9e7c
Troy Smith <fencerman2@gmail.com>
2016-04-25 16:45:56 -0400

    added photos to player module

--- vaultkey/player/models.py
+++ vaultkey/player/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Player(models.Model):
     player_name = models.CharField(max_length=100);
     player_description = models.TextField();
+    image = models.FileField(blank=True, null=True);
 
     def __unicode__(self):
         return self.player_name
